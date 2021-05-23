590e928ac49f9f42c3ecc659f12caacfd2a1fe22
Penguin <t.meng3@newcastle.ac.uk>
2021-05-23 11:03:30 +0800

    modifyed line fx script

--- Assets/Scripts/FX/Line.cs
+++ Assets/Scripts/FX/Line.cs
@@ -8,6 +8,8 @@ public class Line : MonoBehaviour
     //[RequireComponent(typeof(LineRenderer))]
     LineRenderer line;
     public GameObject target;
+    public Vector3 originPosOffset;
+    public Vector3 targetPosOffset;
     public Vector2 postionOffset;
     public Vector2 width = new Vector2(1,0);
     public Color startColor = Color.white;
@@ -24,10 +26,13 @@ public class Line : MonoBehaviour
     {
         if(line && target)
         {
+            Vector3 originPos = originPosOffset + this.transform.position;
+            Vector3 targetPos = targetPosOffset + target.transform.position;
+
             line.positionCount = 2;
-            Vector3 direction = target.transform.position - this.transform.position;
-            line.SetPosition(0,this.transform.position + direction * postionOffset.x);
-            line.SetPosition(1,target.transform.position - direction * postionOffset.y);
+            Vector3 direction = targetPos - originPos;
+            line.SetPosition(0,originPos + direction * postionOffset.x);
+            line.SetPosition(1,targetPos - direction * postionOffset.y);
             line.startWidth = width.x;
             line.endWidth = width.y;
             line.startColor = startColor;
