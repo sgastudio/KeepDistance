0104a9bb847440a52240d56b2715512b50f2bd98
Penguin <t.meng3@newcastle.ac.uk>
2021-05-25 13:41:43 +0800

    Add ROA(ReadOnlyAttribute)

---
+++ Assets/Scripts/System/ROA.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// Read Only Attribute
+/// </summary>
+public class ROA : PropertyAttribute
+{
+
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(ROA))]
+public class ROADrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property,GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
+#endif
