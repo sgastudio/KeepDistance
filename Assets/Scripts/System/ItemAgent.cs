b32b36d9281d80619218151479cabd6be50a5d0c
Penguin <t.meng3@newcastle.ac.uk>
2021-05-27 15:50:50 +0800

    add script to control hold items in scene

---
+++ Assets/Scripts/System/ItemAgent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAgent : MonoBehaviour
+{
+    public string itemName;
+    public int amount = 1;
+    public ItemType type;
+}
