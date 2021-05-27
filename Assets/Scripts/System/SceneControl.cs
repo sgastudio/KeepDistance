5e20ca53991a8a6bd9ca399a3db346d32bd41784
Penguin <t.meng3@newcastle.ac.uk>
2021-05-27 09:25:35 +0800

    rename script

--- Assets/Scripts/System/SceneControl.cs
+++ Assets/Scripts/System/SceneControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Manager_Scene : MonoBehaviour
+public class SceneControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,10 @@ public class Manager_Scene : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnDestroy()
+    {
+        return;
     }
 }
