0ba57a65c9941659c6c11a1e3bd59ca548577830
Penguin <t.meng3@newcastle.ac.uk>
2021-05-28 14:23:01 +0800

    change interactor detector base class

--- Assets/Scripts/System/Detectors/CollisionDetector.cs
+++ Assets/Scripts/System/Detectors/CollisionDetector.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CollisionDetector : MonoBehaviour
 {
+    public enum FilterMode
+    {
+        Or,
+        And
+    }
+    [Header("Filter")]
+    public LayerMask layers;
+    public List<EnumTag> tags;
+    public FilterMode layerTagBlendMode;
+
+    [Header("Event")]
+    public UnityEvent targetEnter;
+    public UnityEvent targetExit;
+
+    [ROA, Space]
+    public List<GameObject> activeList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeList.Clear();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (blendTest(other))
+        {
+            activeList.Remove(other.gameObject);
+            targetExit.Invoke();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (blendTest(other))
+        {
+            activeList.Add(other.gameObject);
+            targetEnter.Invoke();
+        }
+    }
+
+    bool blendTest(Collider other)
+    {
+        switch (layerTagBlendMode)
+        {
+            case FilterMode.And:
+                if (GetTagTest(other.gameObject) && GetLayerTest(other.gameObject.layer))
+                    return true;
+                break;
+            case FilterMode.Or:
+                if (GetTagTest(other.gameObject) || GetLayerTest(other.gameObject.layer))
+                    return true;
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+    public bool GetLayerTest(LayerMask l)
+    {
+        return (layers & l) != 0;
+    }
+
+    public bool GetTagTest(GameObject obj)
+    {
+        bool Result = false;
+
+        foreach (EnumTag tag in tags)
+        {
+            Result |= obj.CompareTag(tag.ToString());
+        }
+        return Result;
     }
 }
--- Assets/Scripts/System/Detectors/InteractDetector.cs
+++ Assets/Scripts/System/Detectors/InteractDetector.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractDetector : MonoBehaviour
+public class InteractDetector : CollisionDetector
 {
     // Start is called before the first frame update
+    [Header("Components")]
     public InventoryManager inventory;
     public PlayerInput input;
-    public string itemTag = "Item";
-    public string switchTag = "Switch";
-    public LayerMask mask;
+    public EnumTag itemTag;
+    public EnumTag switchTag;
+    
+    [Header("Input")]
     public float interactDelay = 0.5f;
-    [ROA]
-    public List<GameObject> interactableList;
     bool interactCooldown = false;
     float LastInteractTime;
     void Start()
@@ -23,26 +24,26 @@ public class InteractDetector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (input.interactAxis > 0 && interactCooldown == false && interactableList.Count > 0)
+        if (input.interactAxis > 0 && interactCooldown == false && activeList.Count > 0)
         {
             interactCooldown = true;
-            if (interactableList.Count > 1)
-                interactableList.Sort(compareDistance);
-            GameObject sceneObj = interactableList[0];
+            if (activeList.Count > 1)
+                activeList.Sort(compareDistance);
+            GameObject sceneObj = activeList[0];
             LastInteractTime = Time.time;
 
             //Add to Inventory
-            if (sceneObj.tag == itemTag)
+            if (sceneObj.tag == itemTag.ToString())
             {
                 ItemAgent agent = sceneObj.GetComponent<ItemAgent>();
                 if (agent)
                     inventory.AddItem(agent.itemName, agent.type, agent.amount, sceneObj);
                 else
                     inventory.AddItem("Item " + sceneObj.GetInstanceID().ToString(), ItemType.Unknown, 1, sceneObj);
-                interactableList.Remove(sceneObj);
+                activeList.Remove(sceneObj);
             }
             //Switch
-            else if (sceneObj.tag == switchTag)
+            else if (sceneObj.tag == switchTag.ToString())
             {
                 SwitchAgent agent = sceneObj.GetComponent<SwitchAgent>();
                 if(agent)
@@ -66,21 +67,5 @@ public class InteractDetector : MonoBehaviour
         float dsty = Vector3.Distance(this.transform.position, y.transform.position);
 
         return (int)(dstx - dsty);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if ((other.CompareTag(itemTag) || other.CompareTag(switchTag)) || (mask & other.gameObject.layer) != 0)
-        {
-            interactableList.Add(other.gameObject);
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if ((other.CompareTag(itemTag) || other.CompareTag(switchTag)) || (mask & other.gameObject.layer) != 0)
-        {
-            interactableList.Remove(other.gameObject);
-        }
     }
 }
