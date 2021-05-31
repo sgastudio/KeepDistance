bfed54250e407046f1a9c559c2a733fdc970979a
Penguin <t.meng3@newcastle.ac.uk>
2021-06-01 02:34:22 +0800

    add goal detector

---
+++ Assets/Scripts/System/Detectors/GoalDetector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalDetector : MonoBehaviour
+//public class GoalDetector : CollisionDetector
+{
+    MissionManager missionManager;
+    public List<string> missionRequirement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        missionManager = GameObject.FindGameObjectWithTag(EnumTag.GameController.ToString()).GetComponent<MissionManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayerReached(Collider player)
+    {
+        
+        Debug.Log("[Player" + player.GetInstanceID().ToString() + "] reaches the goal");
+        bool allMission = missionManager.CheckAllMissionCompleted(missionRequirement.ToArray());
+        Debug.Log("All Mission - " + allMission.ToString());
+    }
+} 
