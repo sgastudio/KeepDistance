e78105f3e1957d2c0f837cc9b6b776daec4791ef
Penguin <t.meng3@newcastle.ac.uk>
2021-06-02 01:29:22 +0800

    improve scene control

--- Assets/Scripts/System/Scene/SceneControl.cs
+++ Assets/Scripts/System/Scene/SceneControl.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneControl : MonoBehaviour
 {
+    public DataManager dataManager;
+    public string nextScene;
+    public string winScene;
+    public string failScene;
     // Start is called before the first frame update
     void Start()
     {
+        dataManager = GameObject.FindWithTag(EnumTag.GameController.ToString()).GetComponent<DataManager>();
         //SceneManager.LoadSceneAsync(1);
     }
 
@@ -24,9 +29,17 @@ public class SceneControl : MonoBehaviour
     public void TriggerFailed()
     {
         Debug.Log("Failed!");
+        if(dataManager)
+            dataManager.SetData("level result",0,"fail",true);
+        if(!string.IsNullOrWhiteSpace(failScene))
+            SceneManager.LoadScene(failScene);
     }
     public void TriggerWin()
     {
         Debug.Log("Win!");
+        if(dataManager)
+            dataManager.SetData("level result",1,"win",true);
+         if(!string.IsNullOrWhiteSpace(winScene))
+            SceneManager.LoadScene(winScene);
     }
 }
