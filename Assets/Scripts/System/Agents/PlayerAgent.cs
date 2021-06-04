42a33a8bdcbf3565127c8e23a115054e7614a47a
Penguin <t.meng3@newcastle.ac.uk>
2021-06-04 13:17:42 +0800

    remove MLAPI

--- Assets/Scripts/System/Agents/PlayerAgent.cs
+++ Assets/Scripts/System/Agents/PlayerAgent.cs
@@ -7,16 +7,11 @@ public class PlayerAgent : MonoBehaviour
     public string playerName;
     public int playerGroup;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        if(string.IsNullOrEmpty(playerName))
+        if (string.IsNullOrEmpty(playerName))
             playerName = this.gameObject.name + this.gameObject.GetHashCode().ToString();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
