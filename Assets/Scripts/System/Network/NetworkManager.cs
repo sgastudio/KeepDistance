8efac19fb72b0fa4b9b0f2a0edfde543635cfdf4
Penguin <t.meng3@newcastle.ac.uk>
2021-06-05 02:19:12 +0800

    add networkmanager

---
+++ Assets/Scripts/System/Network/NetworkManager.cs
@@ -0,0 +1,78 @@
+using System.Runtime.CompilerServices;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    public string gameVersion = "0.1";
+
+    /// <summary>
+    /// The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created.
+    /// </summary>
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClientState state = PhotonNetwork.NetworkClientState;
+        //make compatibility for self-hold sever
+        PhotonNetwork.NetworkingClient.LoadBalancingPeer.SerializationProtocolType = ExitGames.Client.Photon.SerializationProtocol.GpBinaryV16;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = gameVersion;
+    }
+
+    public void CreateRoom() 
+    {
+         PhotonNetwork.CreateRoom(null, new RoomOptions{MaxPlayers = maxPlayersPerRoom,});
+    }
+
+    public void Disconnect()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Connected and calling OnConnectedTOMaster()");
+        //PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN called OnJoinRandomFailed(), but no room avaliable");
+       
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN called OnJoinedRoom(), Room joined");
+    }
+
+    public void SetNickName(string name)
+    {
+        PhotonNetwork.NickName = name;
+    }
+}
