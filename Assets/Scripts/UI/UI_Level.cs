a510f0a49520fc1ea1d2390069144d7367a236da
Penguin <t.meng3@newcastle.ac.uk>
2021-06-10 14:44:54 +0800

    add testing code

--- Assets/Scripts/UI/UI_Level.cs
+++ Assets/Scripts/UI/UI_Level.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 public class UI_Level : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public NetworkManager networkManager;
+    public GameObject playerPrefab;
+
     void Start()
     {
-        
+       GameObject controller = GameObject.FindGameObjectWithTag(EnumTag.GameController.ToString());
+        if (!networkManager && controller)
+            networkManager = controller.GetComponent<NetworkManager>();
+    }
+    public void TriggerTest1()
+    {
+        networkManager.TestCreate();
+    }
+    public void TriggerTest2()
+    {
+        networkManager.TestJoin();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TriggerTest3()
     {
-        
+        PhotonNetwork.Instantiate(playerPrefab.name,Vector3.zero,Quaternion.identity,0);
     }
 }
