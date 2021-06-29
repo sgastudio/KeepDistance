7384a27f1060fcfeb6978c3ac1fd46de13836098
Penguin <t.meng3@newcastle.ac.uk>
2021-06-30 06:19:23 +0800

    edit mission generator

--- Assets/Scripts/System/Mission/MissionGenerator.cs
+++ Assets/Scripts/System/Mission/MissionGenerator.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MissionGenerator : MonoBehaviour
 {
+    public enum ApplyMode
+    {
+        overwrite,
+        additive
+    }
+    public List<MissionContent> missionList;
+    public ApplyMode missionApplyMode;
+    public MissionManager missionManager;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject controller = GameObject.FindGameObjectWithTag(EnumTag.GameController.ToString());
+        if (!missionManager && controller)
+            missionManager = controller.GetComponent<MissionManager>();
+
+        if (!missionManager)
+            Debug.LogError(this + " missing component reference of MissionManager");
+        else
+            if (missionApplyMode == ApplyMode.overwrite)
+        {
+            missionManager.ClearMissions();
+            missionManager.missionList.AddRange(this.missionList);
+        }
+        else
+            missionManager.missionList.AddRange(this.missionList);
     }
 }
