961c159b140b77e31cb4e932f8075f0c3ecd848c
Penguin <t.meng3@newcastle.ac.uk>
2021-07-03 08:43:45 +0800

    prepare for settings

---
+++ Assets/Scripts/System/Settings/SettingsManager.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Photon;
+using Photon.Pun;
+using System.IO;
+
+public class SettingsManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoadSettings()
+    {
+
+    }
+
+    public void SaveSettings()
+    {
+        
+    }
+}
