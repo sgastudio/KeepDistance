beb38e87ccdc548fdb3153f42618e4447c985795
Penguin <t.meng3@newcastle.ac.uk>
2021-07-09 19:44:51 +0800

    Add script to handle UI layers in OVERLAY mode

---
+++ Assets/Scripts/System/Agents/UILevelAgent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILevelAgent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.SetAsLastSibling();
+    }
+}
