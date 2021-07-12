a199931102376737f461fe9af84f0ae13b43dd06
Penguin <t.meng3@newcastle.ac.uk>
2021-07-13 06:14:52 +0800

    functioning itemspawner

--- Assets/Scripts/System/Spawner/ItemSpawner.cs
+++ Assets/Scripts/System/Spawner/ItemSpawner.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class ItemSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject prefab;
+    public Vector3 positionOffset;
+    public Vector3 spawnForce;
+    public Transform spawnPointTransform;
+    public bool spawnOnStart;
+
     void Start()
     {
-        
+        if (!spawnPointTransform)
+            spawnPointTransform = this.transform;
+        if(spawnOnStart)
+            Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Spawn()
+    {
+        GameObject obj;
+        if (prefab)
+        {
+            obj = GameObject.Instantiate(prefab, spawnPointTransform.position + positionOffset, spawnPointTransform.rotation);
+            Rigidbody rigidbody = obj.GetComponent<Rigidbody>();
+            if(rigidbody)
+                rigidbody.AddForce(spawnForce, ForceMode.Impulse);
+        }
     }
 }
