8c49f36c9f86d653693716d795d7afa28977ba44
Penguin <t.meng3@newcastle.ac.uk>
2021-07-15 07:30:07 +0800

    add animator manager to chibi characters

---
+++ Assets/Chibi_characters/ChibiAnimatorManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ChibiAnimatorManager : MonoBehaviour
+{
+    public Animator animator;
+    public PlayerInput playerInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!animator)
+            animator = this.GetComponentInChildren<Animator>();
+        if (!playerInput)
+            playerInput = this.GetComponent<PlayerInput>();
+
+        if (!animator)
+            Debug.LogError(this.ToString() + "Missing Animator Component!");
+        if (!animator)
+            Debug.LogError(this.ToString() + "Missing PlayerInput Component!");
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (animator && playerInput)
+        {
+            // if(playerInput.positionAxis.magnitude != 0)
+            // {
+            //animator.SetFloat("Forward", playerInput.adjustedVelocity.magnitude);
+            animator.SetFloat("Right", playerInput.adjustedVelocity.x);
+            animator.SetFloat("Forward", playerInput.adjustedVelocity.z);
+            //animator.SetFloat("Turn", playerInput.adjustedVelocity.x);
+            // }
+            // else
+            // {
+            //     animator.SetFloat("Forward", 0);
+            //     animator.SetFloat("Right", 0);
+            // }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (!animator)
+            animator = this.GetComponentInChildren<Animator>();
+        if (!playerInput)
+            playerInput = this.GetComponent<PlayerInput>();
+    }
+}
