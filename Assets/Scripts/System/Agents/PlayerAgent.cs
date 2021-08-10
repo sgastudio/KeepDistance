afeb250dd9409e0e8fcf79a2f28da666fcbf190e
Penguin <t.meng3@newcastle.ac.uk>
2021-08-11 05:53:15 +0800

    add particle effect control function

--- Assets/Scripts/System/Agents/PlayerAgent.cs
+++ Assets/Scripts/System/Agents/PlayerAgent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,9 @@ public class PlayerAgent : MonoBehaviourPun
 {
     public string playerName;
     public int playerGroup;
+
+    public ParticleSystem virusParticle;
+    public ParticleSystem cleanParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +27,7 @@ public class PlayerAgent : MonoBehaviourPun
 
         if (camera)
         {
-            if(photonView.IsMine)
+            if (photonView.IsMine)
                 camera.TrackingObject(this.gameObject);
         }
         else
@@ -31,4 +35,43 @@ public class PlayerAgent : MonoBehaviourPun
             Debug.LogError("PlayerInput Trying to bind camera, but missing CameraOperation script on main camera");
         }
     }
+
+    void SwitchParticleSystem(ParticleSystem particleSystem, bool state = true)
+    {
+        if (particleSystem)
+            if (state)
+                particleSystem.Play();
+            else
+                particleSystem.Stop();
+    }
+
+    [PunRPC]
+    void SwitchVirusEffect(bool state)
+    {
+        SwitchParticleSystem(virusParticle, state);
+    }
+
+    [PunRPC]
+    void SwitchCleanEffect()
+    {
+        SwitchParticleSystem(cleanParticle, true);
+    }
+
+
+    public void TriggerVirusEffect(bool state)
+    {
+        if (PhotonNetwork.OfflineMode)
+            SwitchVirusEffect(state);
+        else
+            photonView.RPC("SwitchVirusEffect", RpcTarget.All, state);
+    }
+
+    public void TriggerCleanEffect()
+    {
+        if (PhotonNetwork.OfflineMode)
+            SwitchCleanEffect();
+        else
+            photonView.RPC("SwitchCleanEffect", RpcTarget.All);
+    }
+
 }
