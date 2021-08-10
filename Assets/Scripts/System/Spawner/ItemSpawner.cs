5999fee6bf3d24a16df6936106619cc6b49c1897
Penguin <t.meng3@newcastle.ac.uk>
2021-08-11 05:50:45 +0800

    add random and loop function

--- Assets/Scripts/System/Spawner/ItemSpawner.cs
+++ Assets/Scripts/System/Spawner/ItemSpawner.cs
@@ -8,21 +8,34 @@ public class ItemSpawner : MonoBehaviour
     public GameObject prefab;
     public Vector3 positionOffset;
     public Vector3 spawnForce;
+    public float forceRandomize;
+    Vector3 forceOffset;
     public Transform spawnPointTransform;
     public bool spawnOnStart;
+
+    public float loopInterval = 0.0f;
+    public float loopRandomize = 0.0f;
+    float intervalOffset;
+    public int loopTimes = 0;
+    int spawnedTimes;
+    float lastSpawnTime;
 
     void Start()
     {
         if (!spawnPointTransform)
             spawnPointTransform = this.transform;
-        if(spawnOnStart)
+        if (spawnOnStart)
             Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (loopInterval > 0 && ((loopTimes > 0 && spawnedTimes < loopTimes) || loopTimes == 0))
+        {
+            if (lastSpawnTime + loopInterval + intervalOffset <= Time.time)
+                Spawn();
+        }
     }
 
     public void Spawn()
@@ -32,8 +45,15 @@ public class ItemSpawner : MonoBehaviour
         {
             obj = GameObject.Instantiate(prefab, spawnPointTransform.position + positionOffset, spawnPointTransform.rotation);
             Rigidbody rigidbody = obj.GetComponent<Rigidbody>();
-            if(rigidbody)
-                rigidbody.AddForce(spawnForce, ForceMode.Impulse);
+            
+            if (rigidbody)
+            {
+                forceOffset = Random.insideUnitSphere * forceRandomize;
+                rigidbody.AddForce(spawnForce + forceOffset, ForceMode.Impulse);
+            }
         }
+        lastSpawnTime = Time.time;
+        spawnedTimes += 1;
+        intervalOffset = Random.Range(-loopRandomize, loopRandomize);
     }
 }
