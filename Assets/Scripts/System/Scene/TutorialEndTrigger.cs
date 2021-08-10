8f602b9415a17aae139f6302a2dd490aa1e27f91
Penguin <t.meng3@newcastle.ac.uk>
2021-08-11 05:54:06 +0800

    add trigger for tutorial finish

---
+++ Assets/Scripts/System/Scene/TutorialEndTrigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEndTrigger : MonoBehaviour
+{
+    NetworkManager networkManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject systemObject = GameObject.FindGameObjectWithTag(EnumTag.GameController.ToString());
+        if(!systemObject)
+        {
+            Debug.LogWarning("System Control Object not detected!");
+            return;
+        }
+        networkManager = systemObject.GetComponent<NetworkManager>();
+        if(!networkManager)
+        {
+            Debug.LogError("Missing Network manager on System Control Object");
+        }
+    }
+
+
+    public void TriggerEnd()
+    {
+        if(networkManager)
+            networkManager.StartMainMenu();
+    }
+}
