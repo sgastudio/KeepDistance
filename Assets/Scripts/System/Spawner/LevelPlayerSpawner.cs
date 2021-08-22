a9cac44e562e85a878f980fa0ed6cff7bc847764
Penguin <t.meng3@newcastle.ac.uk>
2021-08-23 06:39:19 +0800

    add position offset function

--- Assets/Scripts/System/Spawner/LevelPlayerSpawner.cs
+++ Assets/Scripts/System/Spawner/LevelPlayerSpawner.cs
@@ -17,14 +17,38 @@ public class LevelPlayerSpawner : MonoBehaviour
         else
             return;
 
-        if (PhotonNetwork.IsConnected || PhotonNetwork.OfflineMode)
+        if (networkManager.localPlayerObject == null)
+            if (PhotonNetwork.IsConnected || PhotonNetwork.OfflineMode)
             {
                 GameObject playerObject = PhotonNetwork.Instantiate(networkManager.gender > 0 ? boyPrefab.name : girlPrefab.name, Vector3.zero, Quaternion.identity, 0);
-                networkManager.localPlayerView = playerObject.GetComponent<PhotonView>().ViewID;
+                networkManager.localPlayerObject = playerObject;
                 PlayerAgent agent = playerObject.GetComponent<PlayerAgent>();
+                PhotonNetwork.LocalPlayer.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { PlayerAgent.PLAYER_GENDER_KEY, (byte)networkManager.gender } });
                 agent.playerName = networkManager.clientNickName;
                 agent.gender = networkManager.gender;
+                agent.spawnOffset = GetIdPositionOffset(PhotonNetwork.LocalPlayer.ActorNumber,10,20,3);
+                agent.RestPostion();
+            }
+            else
+            {
+                networkManager.localPlayerObject.transform.position = Vector3.up * 2;
             }
 
+    }
+
+    Vector2 GetIdPositionOffset(int id, int maxPerline, int maxLimit, int width, bool centered = true)
+    {
+        int lines = maxLimit % maxPerline != 0 ? maxLimit / maxPerline + 1 : maxLimit / maxPerline;
+
+        int posX = id % maxPerline;
+        int posY = id / maxPerline;
+
+        if (centered)
+        {
+            posX -= maxPerline / 2;
+            posY -= lines / 2;
+        }
+
+        return new Vector2(width * posX, width * posY);
     }
 }
