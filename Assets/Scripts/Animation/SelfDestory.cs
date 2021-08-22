b3b3e08096830ad737871b6fa8586bb28783174b
Penguin <t.meng3@newcastle.ac.uk>
2021-08-23 04:24:20 +0800

    add game object self destory script

---
+++ Assets/Scripts/Animation/SelfDestory.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestory : MonoBehaviour
+{
+    public float duration;
+    float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Destroy(this.gameObject,duration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
