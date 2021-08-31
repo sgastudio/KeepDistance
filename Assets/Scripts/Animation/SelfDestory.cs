26f76ba2c48edf7c23f6ca038b707d344dc8fb19
Penguin <t.meng3@newcastle.ac.uk>
2021-09-01 05:47:37 +0800

    change behaviour using coroutine

--- Assets/Scripts/Animation/SelfDestory.cs
+++ Assets/Scripts/Animation/SelfDestory.cs
@@ -7,15 +7,27 @@ public class SelfDestory : MonoBehaviour
 {
     public float duration;
     float startTime;
+    public ParticleSystem effect;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Destroy(this.gameObject,duration);
+        StartCoroutine("SelfDestruction");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator SelfDestruction()
+    {
+        yield return new WaitForSeconds(duration);
+        if (effect)
+        {
+            effect.transform.SetParent(null);
+            effect.Play();
+        }
+        GameObject.Destroy(this.gameObject);
     }
 }
