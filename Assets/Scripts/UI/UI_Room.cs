74a06d67ceabcf8089e39bb94290eb5f1573f795
Penguin <t.meng3@newcastle.ac.uk>
2021-09-01 06:01:08 +0800

    change ui logic

--- Assets/Scripts/UI/UI_Room.cs
+++ Assets/Scripts/UI/UI_Room.cs
@@ -3,25 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class UI_Room : StackPanel
 {
-    #region public triggers
-        public void triggerBack()
-        {
-            if(networkManager)
-                networkManager.LeaveRoom();
-        }
+    public UnityEngine.UI.Button startButton;
 
-        public void triggerStart()
+    public override void Start()
+    {
+        base.Start();
+        if (startButton)
+            if (!PhotonNetwork.IsMasterClient)
+                startButton.enabled = false;
+            else
+                startButton.enabled = true;
+    }
+
+    #region public triggers
+    public void triggerBack()
+    {
+        if (networkManager)
         {
-            if(networkManager)
-                networkManager.StartArena();
+            networkManager.LeaveRoom();
+            networkManager.Disconnect();
         }
+    }
+
+    public void triggerStart()
+    {
+        if (networkManager && PhotonNetwork.IsMasterClient)
+            networkManager.StartArena();
+    }
     #endregion
 
     public override void OnLeftRoom()
     {
+        base.OnLeftRoom();
+
         TriggerLastPanel();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+
+        //TriggerLastPanel();
     }
 }
