d5ab32f2fac391f879371c21fcca65ffb72ca5d5
Penguin <t.meng3@newcastle.ac.uk>
2021-09-01 06:01:34 +0800

    remove necessary entries

--- Assets/Scripts/UI/UI_Main.cs
+++ Assets/Scripts/UI/UI_Main.cs
@@ -7,17 +7,6 @@ public class UI_Main : StackPanel
 {
     //public UnityEvent onPlay;
     //public UnityEvent onOption;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void triggerPlay()
     {
